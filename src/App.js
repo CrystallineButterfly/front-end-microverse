a034883dc721a23664309cfdb8935393fb3a3aea
Kell McWatt <fromtc2k42@gmail.com>
2022-11-10 22:17:16 +0000

    Add linters to over-complicate the simplicity of a simple app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import store from "./redux/store";
-import Greetings from "./components/greetings";
 import { Provider } from 'react-redux';
+import store from './redux/store';
+import Greetings from './components/greetings';
 
 function App() {
   return (
     <Provider store={store}>
-     <Greetings/>
+      <Greetings />
     </Provider>
   );
 }
