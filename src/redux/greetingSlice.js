eb2c2a2caf610f2135492b257b0648cd8f5f178a
Kell McWatt <fromtc2k42@gmail.com>
2022-11-10 22:24:21 +0000

    Yes expected

--- src/redux/greetingSlice.js
+++ src/redux/greetingSlice.js
@@ -1,18 +1,19 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+/* eslint-disable */
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const fetchGreeting = createAsyncThunk(
-  'greeting/fetchGreeting',
+  "greeting/fetchGreeting",
   async () => {
-    const response = await fetch('http://localhost:3004/api/v1/greeting');
+    const response = await fetch("http://localhost:3004/api/v1/greeting");
     const greeting = await response.json();
     return greeting;
-  },
+  }
 );
 
 export const greetingSlice = createSlice({
-  name: 'greeting',
+  name: "greeting",
   initialState: {
-    greeting: 'Hello beings of the universe!',
+    greeting: "Hello beings of the universe!",
   },
   reducers: {},
   extraReducers: {
